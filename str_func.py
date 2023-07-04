e04a0f58be9ec776b82da02e8ebfa9ed49d2a3c0
gololeilo <137124675+gololeilo@users.noreply.github.com>
2023-07-04 21:52:00 +1000

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@
 def bigwords():
    s1 = 'abcdef'
    s2 = s1.upper() # s2 = 'ABCDEF'
+
+# Функция title()
+'''первые буквы заглавные'''
+def first_bigwords():
+   s1 = 'abcdef'
+   s2 = s1.title() # s2 = 'Abcdef'
